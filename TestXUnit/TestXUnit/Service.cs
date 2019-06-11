4e3b709acefbbec49b0f72bf656e599979dd877f
jiohelps <31577170+jiolearn@users.noreply.github.com>
2019-06-11 23:13:21 +1000

    xunit code

---
+++ TestXUnit/TestXUnit/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitDemo
+{
+    public class Service
+    {
+        public int AddSalary(int basic,int super)
+        {
+            int total = basic + super;
+            return total;
+        }
+    }
+}
---
+++ TestXUnit/TestXUnit/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using xUnitDemo;
+
+namespace Test
+{
+    public class TestClass
+    {
+        [Theory]
+        [InlineData(10000, 20000, 30000)]
+        [InlineData(10000, 20000, 50000)]
+        public void TestService(int basic, int super, int expected)
+        {
+            Service sc = new Service();
+            
+            int total = sc.AddSalary(basic, super);
+
+            Assert.Equal(total, expected);
+        }
+        
+    }
+}
