bbe2d0d71a1017ed50658dac60fa037f825ebbaa
jiohelps <31577170+jiolearn@users.noreply.github.com>
2019-06-16 14:57:52 +1000

    Updated
    Updated

---
+++ TestXUnit/Employee.Services/Service.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Employee.Services
+{
+    public class Service
+    {
+        
+
+        public int AddSalary(int basic, int super)
+        {
+            int total = basic + super;
+            return total;
+        }
+
+        public StreamReader ReadExcel()
+        {
+            var reader = new StreamReader(@"E:\Learn\TestXUnit\SalTest.csv");
+            
+
+                return reader;
+            
+        }
+
+        public static IEnumerable<object[]> ReadValues()
+        { 
+
+            List<object[]> ExcelDataValues=new List<object[]>();
+
+            var reader = new StreamReader(@"E:\Learn\TestXUnit\SalTest.csv");
+
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                //var values = line.Split(',');
+                //ExcelDataValues.Add(new object[] { line });
+
+               string[]  lineVal = line.Split(',');
+
+                yield return new object[] { lineVal[0], lineVal[1], lineVal[2] };
+            }
+
+            //return ExcelDataValues;
+        }
+
+        
+
+    }
+}
--- TestXUnit/TestXUnit/TestClass.cs
+++ TestXUnit/TestXUnit/TestClass.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Employee.Services;
 using Xunit;
-using xUnitDemo;
+using ExcelDataReader;
+using System.IO;
+using System.Data;
+//using Excel=Microsoft.Office.Tools.Excel;
+using System;
+using System.Collections.Generic;
 
 namespace Test
 {
+
+    
+
     public class TestClass
     {
+              
+        
+        public static IEnumerable<object[]> GridValues()
+        {
+
+            return Service.ReadValues();
+
+           
+        }
+
+
+
         [Theory]
-        [InlineData(10000, 20000, 30000)]
-        [InlineData(10000, 20000, 50000)]
+        [MemberData(nameof(GridValues))]
         public void TestService(int basic, int super, int expected)
         {
+            
+
             Service sc = new Service();
+
+            sc.ReadExcel();
             
             int total = sc.AddSalary(basic, super);
 
